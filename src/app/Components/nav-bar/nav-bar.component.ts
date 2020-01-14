93912678f96e73f35eec84b1f337de7d62a6759d
Alan <15030093@itcelaya.edu.mx>
2020-01-13 21:23:24 -0600

    Mejora de responsive

--- src/app/Components/nav-bar/nav-bar.component.ts
+++ src/app/Components/nav-bar/nav-bar.component.ts
@@ -12,17 +12,30 @@ export class NavBarComponent implements OnInit {
     const burger: HTMLElement = document.querySelector('.burger');
     const nav: HTMLElement = document.querySelector('.nav-links');
     const navLinks: NodeListOf<HTMLElement> = document.querySelectorAll('.nav-links li');
+    const links: NodeListOf<HTMLElement> = document.querySelectorAll('.link');
+    links.forEach( (link) => {
+      link.addEventListener('click', () => {
+        if (nav.classList.contains('nav-active')) {
+          nav.classList.toggle('nav-active');
+          this.animateLinks(navLinks);
+          burger.classList.toggle('toggle');
+        }
+        window.scrollTo({ left: 0, top: 0, behavior: 'smooth'});
+      });
+    });
     burger.addEventListener('click', () => {
       nav.classList.toggle('nav-active');
-      navLinks.forEach((  link, index) => {
-        if (link.style.animation) {
-          link.style.animation = '';
-        } else {
-          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-        }
-      });
+      this.animateLinks(navLinks);
       burger.classList.toggle('toggle');
     });
   }
-
+  animateLinks(navLinks) {
+    navLinks.forEach((  link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.25s ease forwards ${index / 7 + 0.5}s`;
+      }
+    });
+  }
 }
